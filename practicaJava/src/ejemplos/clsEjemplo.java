cb9615d5f995deb2b0d90267adb977e39dcc2e56
jpablo79 <juanp_rgonzalez@hotmail.com>
2022-08-13 17:36:39 -0600

    ejemplo de clase(comentarios)

---
+++ practicaJava/src/ejemplos/clsEjemplo.java
@@ -0,0 +1,16 @@
+package ejemplos;
+
+
+public class clsEjemplo {
+    // lista de atributos
+    // objetos = new clase();
+    
+    
+    
+    // lista de constructor o constructores
+    //nombreMetodo (int, string....)
+        //this.atributo = parametros;
+    
+    //lista de meto o metodos
+    
+}
